192f15bb47232cdd476b166922b986b1163874fa
ViartFelix <viart.felix2017@gmail.com>
2024-01-29 13:39:59 +0100

    v0.3.5: share buttons

--- assets/js/recipe/single/index.js
+++ assets/js/recipe/single/index.js
@@ -1,15 +1,22 @@
 import $ from "jquery"
-import Elements from "./elements";
+import "share-buttons"
 
 $(()=>{
-	//new Main()
-	new a()
+	new Main()
 })
 
-class a
+class Main
 {
 	constructor() {
+		this.hydrate()
+	}
+
+	hydrate()
+	{
 		$(".add_item_link").on("click", this.addFormToCollection)
+
+		let shareContainer = $("#to-share");
+
 	}
 
 	addFormToCollection(event)
@@ -33,56 +40,3 @@ class a
 		collectionHolder.dataset.index++;
 	}
 }
-
-class Main
-{
-
-	elements;
-	lastID=0;
-
-	constructor() {
-		this.elements = new Elements();
-		this.hydrate()
-	}
-
-	hydrate()
-	{
-		this.elements.addBtn.on("click", (event)=>{
-			event.preventDefault()
-			this.addInstruction()
-		})
-	}
-
-	addInstruction()
-	{
-
-		this.elements.addTem
-			.clone()
-			.find("textarea")
-			.attr("name", "form[instructions]["+this.getLastID()+"]")
-			.parent()
-			.appendTo("#instructions")
-
-		this.incrementLastID();
-	}
-
-
-	getLastID()
-	{
-		this.lastID = parseInt($("#__last-ID__").html()) ?? 0
-		return this.lastID;
-	}
-
-	incrementLastID()
-	{
-		this.lastID++;
-		$("#__last-ID__").html(this.lastID);
-	}
-
-}
-
-/**
- * <div class="hidden" style="display: none" id="add-instruction">
- * 			{{ form_row(form.instructions.vars.prototype) }}
- * 		</div>
- */
